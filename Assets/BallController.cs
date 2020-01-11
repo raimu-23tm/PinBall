42b2cf1bef6c4ea578fcbd9683e3ff84c637e97a
Yo Sato <raimu_23tm@hotmail.co.jp>
2020-01-11 22:29:27 +0900

    PinBallをコミット
    課題と発展課題を実装

---
+++ Assets/BallController.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.7f;
+
+    //テキストオブジェクト
+    private GameObject gameoverText;
+    private GameObject pointText;
+
+    // 現在の得点
+    public static int currentPoint;
+
+    // ゲームオーバー状態フラグ
+    private bool gameOverFlg;
+
+    private Rigidbody _rigidbody;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        //テキストオブジェクト取得
+        this.gameoverText = GameObject.Find("GameOverText");
+        this.pointText = GameObject.Find("PointText");
+
+        //初期設定
+        currentPoint = 0;
+        gameOverFlg = false;
+        _rigidbody = this.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+            gameOverFlg = true;
+            _rigidbody.velocity = Vector3.zero;
+
+        }
+
+        if (gameOverFlg == true)
+        {
+            //スペースキーを押すとリトライ
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                RetryGame();
+            }
+
+            //（スマホの場合）タッチするとリトライ
+            foreach (Touch t in Input.touches)
+            {
+                var id = t.fingerId;
+
+                switch (t.phase)
+                {
+                    case TouchPhase.Began:
+
+                        RetryGame();
+                        break;
+                }
+            }
+
+        }
+
+        //超速防止
+        if (_rigidbody.velocity.magnitude > 20)
+        {
+            Vector3 currentVelo = _rigidbody.velocity;
+            currentVelo = currentVelo * (20 / _rigidbody.velocity.magnitude);
+            _rigidbody.velocity = currentVelo;
+        }
+
+    }
+
+    // Update is called once per frame
+    void RetryGame()
+    {
+        this.gameoverText.GetComponent<Text>().text = "";
+        this.pointText.GetComponent<Text>().text = "Point: 0";
+        gameOverFlg = false;
+        _rigidbody.velocity = Vector3.zero;
+        currentPoint = 0;
+        this.transform.position = new Vector3(3f, 2.8f, 4f);
+    }
+
+}
---
+++ Assets/BrightnessRegulator.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BrightnessRegulator : MonoBehaviour {
+
+    // Materialを入れる
+    Material myMaterial;
+
+    // Emissionの最小値
+    private float minEmission = 0.3f;
+    // Emissionの強度
+    private float magEmission = 2.0f;
+    // 角度
+    private int degree = 0;
+    //発光速度
+    private int speed = 10;
+    // ターゲットのデフォルトの色
+    Color defaultColor = Color.white;
+
+    //得点テキスト
+    private GameObject pointText;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // タグによって光らせる色を変える
+        if (tag == "SmallStarTag")
+        {
+            this.defaultColor = Color.white;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            this.defaultColor = Color.yellow;
+        }
+        else if (tag == "SmallCloudTag" || tag == "LargeCloudTag")
+        {
+            this.defaultColor = Color.cyan;
+        }
+
+        //オブジェクトにアタッチしているMaterialを取得
+        this.myMaterial = GetComponent<Renderer>().material;
+
+        //オブジェクトの最初の色を設定
+        myMaterial.SetColor("_EmissionColor", this.defaultColor * minEmission);
+
+        //テキストオブジェクト取得
+        this.pointText = GameObject.Find("PointText");
+
+        this.pointText.GetComponent<Text>().text = "Point: 0";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (this.degree >= 0)
+        {
+            // 光らせる強度を計算する
+            Color emissionColor = this.defaultColor * (this.minEmission + Mathf.Sin(this.degree * Mathf.Deg2Rad) * this.magEmission);
+
+            // エミッションに色を設定する
+            myMaterial.SetColor("_EmissionColor", emissionColor);
+
+            //現在の角度を小さくする
+            this.degree -= this.speed;
+        }
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        //角度を180に設定
+        this.degree = 180;
+
+        // タグによって光らせる色を変える
+        if (tag == "SmallStarTag")
+        {
+            BallController.currentPoint += 1;
+        }
+        else if (tag == "LargeStarTag")
+        {
+            BallController.currentPoint += 5;
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            BallController.currentPoint += 10;
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            BallController.currentPoint += 50;
+        }
+
+        this.pointText.GetComponent<Text>().text = "Point: " + BallController.currentPoint;
+
+    }
+
+}
